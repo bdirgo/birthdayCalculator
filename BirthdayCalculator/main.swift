391761d3c7e7b1a55c9b7c599988609d87dec19e
Benjamin Dirgo <bdirgo@gmail.com>
2020-05-25 17:00:46 -0700

    Initial Commit

---
+++ BirthdayCalculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BirthdayCalculator
+//
+//  Created by Benjamin Dirgo on 5/25/20.
+//  Copyright © 2020 Benjamin Dirgo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
